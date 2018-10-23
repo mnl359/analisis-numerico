11b035d896dca9e1c481f21ddfe7639344a26858
Manuela Carrasco <mcarras1@eafit.edu.co>
2018-10-23 07:08:47 -0500

    Lagrange finished

--- Project/Polynomial_Interpolation/lagrange.py
+++ Project/Polynomial_Interpolation/lagrange.py
@@ -1,31 +1,38 @@
 #!/bin/python3
 
-res = []
+from sympy import Symbol, expand, simplify, factor, init_printing
 
 def l(value, i, matrix):
-    global res
-    x = 0
+    init_printing(use_unicode=True)
+    x = Symbol("x")
+    z = 0
     num = 1
-    while x < len(matrix):
-        if x != i:
-            num = num * (value - matrix[x][0])
-        x+=1
-    print(num)
     y = 0
     den = 1
     while y < len(matrix):
-        if x != i:
-            num = num * (matrix[i][0] - matrix[y][0])
+        if y != i:
+            den = den * (matrix[i][0] - matrix[y][0])
         y+=1
-    return num/den
+    while z < len(matrix):
+        if z != i:
+            aux = expand(x - matrix[z][0])
+            num = expand(num * aux)
+        z+=1
+    aux = factor(num/den)
+    ln = aux.evalf(subs={x:value})
+    return aux, ln
 
 def lagrange(x, matrix):
     pol = 0
+    pol1 = 0
     for i in range(len(matrix)):
         ln = l(x, i, matrix)
-        pol += ln*matrix[i][1]
+        pol += ln[1]*matrix[i][1]
+        pol1 += ln[0]*matrix[i][1]
+    print("El polinomio resultante es:")
+    print(pol1)
     return pol
 
 m = [[2, -4.610943901069350],[2.2, -4.174986500565880],[2.4, -3.376823619358400],[2.6, -2.136261964998310],[2.8, -0.355353228902949]]
 value = 2.5
-print(lagrange(value, m))
+print("El resultado de p(%s) es igual a %s" % (value, lagrange(value, m)))
