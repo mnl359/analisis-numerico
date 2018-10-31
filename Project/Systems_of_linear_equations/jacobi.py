a498b5773db03f5bfb76d94b637848d5199651a9
Manuela Carrasco <mcarras1@eafit.edu.co>
2018-10-31 13:13:50 -0500

    Jacobi finished

--- Project/Systems_of_linear_equations/jacobi.py
+++ Project/Systems_of_linear_equations/jacobi.py
@@ -5,15 +5,16 @@ from sympy import symbols, init_printing
 from math import sqrt
 from copy import copy
 
-def newJacobi(x):
-    init_printing(use_unicode=True)
-    x0, x1, x2 = symbols('x0, x1, x2')
-    functions = [(-23+(4*x1)+(5*x2))/13, (5-(3*x0)-(2*x2))/(-7), (34+(4*x0)-(5*x1))/(-16)]
-    result = []
-    for i in functions:
-        aux = i.evalf(subs={x0:x[0], x1:x[1], x2:x[2]})
-        result.append(aux)
-    return result
+def newJacobi(x0, matrix, b):
+    aux = []
+    for i in range(len(matrix)):
+        suma = 0
+        for j in range(len(matrix)):
+            if j != i:
+                suma += matrix[i][j]*x0[j]
+        suma = (b[i] - suma)/matrix[i][i]
+        aux.append(suma)
+    return aux
 
 def norma(x0, x1):
     sum = 0
@@ -24,7 +25,7 @@ def norma(x0, x1):
     error = (sqrt(sum))/(sqrt(den))
     return error
 
-def jacobi(tolerance, x0, iterations):
+def jacobi(tolerance, x0, iterations, matrix, b):
     title = ['n']
     aux = 0
     cont = 0
@@ -36,12 +37,14 @@ def jacobi(tolerance, x0, iterations):
     table.add_row([cont] + x0 + ["Doesn't exist"])
     error = tolerance + 1
     while error > tolerance and cont < iterations:
-        x1 = newJacobi(x0)
+        x1 = newJacobi(x0, matrix, b)
         error = norma(x0, x1)
         cont += 1
         table.add_row([cont] + x1 + [error])
         x0 = copy(x1)
     print(table)
 
-jacobi(5e-06, [0,0,0], 20)
+m = [[13, -4, -5],[3, -7, 2],[-4, 5, -16]]
+b = [-23, 5 ,34]
+jacobi(5e-06, [0,0,0], 20, m, b)
     
