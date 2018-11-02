46a48d423d8cdc2ceecbd2da3dc69f82e9c85340
Manuela Carrasco <mcarras1@eafit.edu.co>
2018-11-02 04:02:58 -0500

    Working

--- Project/Polynomial_Interpolation/newton.py
+++ Project/Polynomial_Interpolation/newton.py
@@ -13,7 +13,7 @@ def bn(iterations):
         res[iterations][x+1] = (res[iterations-1][x] - res[iterations][x])/(res[y][0] - res[iterations][0])
         y -= 1
 
-def newton(value, matrix):
+def newton(value, matrix, txt):
     global res
     init_printing(use_unicode=True)
     x = Symbol("x")
@@ -34,15 +34,28 @@ def newton(value, matrix):
     for iteration in range(1,n):
         bn(iteration)
         b = res[iteration][iteration+1]
-        mult = expand(mult * (x - matrix[iteration][0]))
+        print(mult * (x - matrix[iteration-1][0]))
+        mult = expand(mult * (x - matrix[iteration-1][0]))
         pol += (b*mult)
         table.add_row(res[iteration])
-    print(table)
-    print("El polinomio resultante es:\n %s" % pol)
+    print(table, file=txt)
+    print("El polinomio resultante es:\n %s" % pol, file=txt)
     result = pol.evalf(subs={x:value})
-    print("El resultado de p(%s) es igual a %s" % (value, result))
+    print("El resultado de p(%s) es igual a %s" % (value, result), file=txt)
     return result, pol
 
-m = [[0, 0],[1, 1],[2, 1],[3, 0]]
+X =[
+    [1.0000, 0.5949],
+    [2.0000, 0.2622],
+    [3.0000, 0.6028],
+    [4.0000, 0.7112],
+    [5.0000, 0.2217],
+    [6.0000, 0.1174],
+    [7.0000, 0.2967],
+    [8.0000, 0.3188],
+    [9.0000, 0.4242],
+   [10.0000, 0.5079]]
 value = 0.5
-newton(value, m)[1]
+#X = [[-4, -2.59625884456571],[-2, -0.696958389857672],[-1, 0.908181747039582]]
+with open("newton.txt", "w") as result:
+    newton(value, X, result)[1]
