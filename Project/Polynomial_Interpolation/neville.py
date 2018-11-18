41212dc10cc76dfb7cebc236f362f880c50ea423
DaniloV <jvasque6@eafit.edu.co>
2018-11-18 08:36:53 -0500

    Fixing neville's output

--- Project/Polynomial_Interpolation/neville.py
+++ Project/Polynomial_Interpolation/neville.py
@@ -3,10 +3,10 @@
 from sympy import Symbol, expand, simplify, factor, init_printing
 import numpy as np
 
-def neville(xy):
+def neville(value, xy):
     x = Symbol("x")
-    n = len(xy[0])
-    check = checkData(xy[0])
+    n = len(xy)
+    check = checkData(xy)
     if(check[0] == 1):
         return(check)
     polys = []
@@ -14,16 +14,17 @@ def neville(xy):
         row = [None]*n
         polys.append(row)
     for i in range(n):
-        polys[i][i] = xy[1][i]
+        polys[i][i] = xy[i][1]
 
     for j in range(1,n):
         for i in range(n-j):
-            xi = xy[0][i]
-            xj = xy[0][j + i]
+            xi = xy[i][0]
+            xj = xy[j + i][0]
             polij = expand(((x - xi) * polys[i+1][j + i] - (x - xj) * polys[i][j-1+i]) / (xj - xi))
             polys[i][j+i] = polij
-
-    return(0, polys[0][n-1])
+    poly = polys[0][n-1]
+    value = evalPoly(value, poly)
+    return(0, poly, value)
 
 def evalPoly(value, poly):
     x = Symbol("x")
@@ -32,17 +33,22 @@ def evalPoly(value, poly):
 def checkData(X):
     n = len(X)
     if(n < 2):
-        return(1, "The set of dots must have at least 2 elements.")
+        return(1, "The set of points must have at least 2 elements.")
     for i in range(n - 1):
-        if(X[i+1] < X[i]):
-            return(1, "The set of dots must be arranged in ascending order with respect to their X component. Problem found at: " + str(i))
-        elif(X[i+1] == X[i]):
-            return(1, "All dots must be different. Problem found at: " + str(i))
+        if(len(X[i]) < 2):
+            return(1, "Every point must have both X and Y components. Problem found at: " + str(i))
+        elif(len(X[i+1]) < 2):
+            return(1, "Every point must have both X and Y components. Problem found at: " + str(i+1))
+        elif(X[i+1][0] < X[i][0]):
+            return(1, "The set of points must be arranged in ascending order with respect to their X component. Problem found at: " + str(i))
+        elif(X[i+1][0] == X[i][0]):
+            return(1, "All points must be different. Problem found at: " + str(i))
     return(0, "Ok.")
 
 #xy = [[0, 0],[1, 1],[2, 1],[3, 0]]
-xy = [[-1, -1, 5],[15, 5, 9]]
+#xy = [[-1, -1, 5],[15, 5, 9]]
+xy = [[-1, 15], [4, 5], [5, 9]]
 value = 5
-poly = neville(xy)
+poly = neville(5, xy)
 #print("The resulting polynomial is: ", poly)
 #print("The result of p(%s) is %s" % (value, evalPoly(value, poly)))
